8bbea14364e91cdde4791bce360c45ba7f6dc4de
krishnamohan-yerrabilli <krishnamohan72452964@gmail.com>
2022-12-27 10:54:36 +0530

    adding logout button and a welcome message only after successfull login

---
+++ public/dashboard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+function Dashboard() {
+  const history = useHistory();
+
+  const handleLogout = () => {
+    fetch('/api/logout', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.success) {
+          history.push('/');
+        }
+      });
+  };
+
+  return (
+    <div>
+      <h1>Welcome to the Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
+    </div>
+  );
+}
+
+export default Dashboard;
